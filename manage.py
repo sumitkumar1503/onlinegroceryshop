8634fb225604688d535458a8fc8d0414ec099e24
sumit <wapka1503@gmail.com>
2023-01-03 21:55:38 +0530

    m

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'homebazaar.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'onlinegroceryshop.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
